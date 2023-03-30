cc2844fa79ab02dd1f601124f5c114f8804d2f58
Jair Andrade <jair.lionsoft@gmail.com>
2023-03-30 11:16:05 -0300

    add post component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './global.css'
 
 import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <h1>POSTS</h1>
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,47 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/joaocamargo06.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Joao Vitor Camargo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time>Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p></p>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href=""> #nlw </a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu comentário</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
