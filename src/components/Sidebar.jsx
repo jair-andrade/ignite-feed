780c1d39cdfe5a4e8d366123511979575afe7f64
Jair Andrade <jair.lionsoft@gmail.com>
2023-04-03 16:53:13 -0300

    add avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 import { PencilLine } from "@phosphor-icons/react";
 
@@ -9,16 +10,13 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/jair-andrade.png"
-        />
+        <Avatar src="https://github.com/joaocamargo06.png" />
         <strong>jair jotajota</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
-          <PencilLine size={20}/>
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
